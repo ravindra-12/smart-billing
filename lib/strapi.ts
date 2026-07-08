41f4fa125c9306549e3af1935ad5f35780e9a81e
ravindra-12 <murmuravindra609@gmail.com>
2026-07-08 19:27:57 +0530

    Build Issue Fixed

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -2,22 +2,25 @@ const STRAPI_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 
 export async function getHomeMeta() {
-  const res = await fetch(
-    `${STRAPI_URL}/api/home-meta?populate[seo][populate]=*&populate[geo][populate]=*`,
-    {
-      next:{
-        revalidate:60
+  try {
+    const res = await fetch(
+      `${STRAPI_URL}/api/home-meta?populate[seo][populate]=*&populate[geo][populate]=*`,
+      {
+        next: {
+          revalidate: 60,
+        },
       }
+    );
+
+    if (!res.ok) {
+      console.error("Failed to fetch Home Meta:", res.status, res.statusText);
+      return null;
     }
-  );
 
-
-  if(!res.ok){
-    throw new Error("Failed to fetch Home Meta");
+    const json = await res.json();
+    return json.data;
+  } catch (err) {
+    console.error("Error fetching Home Meta:", err);
+    return null;
   }
-
-
-  const json = await res.json();
-
-  return json.data;
-}
+}
